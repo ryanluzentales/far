766102eeb5b85c316d2fe7e62c60ae00b441ebca
Roberto Sy Jr <syrobertojr2001@gmail.com>
2022-10-24 21:51:51 +0800

    rearranged the sidebar for the owner page
    rearranged the sidebar for the owner page

--- owner/dashboard.php
+++ owner/dashboard.php
@@ -6,92 +6,98 @@ if (strlen($_SESSION['ologin']) == 0) {
 	header('location:index.php');
 } else {
 ?>
-	<!doctype html>
-	<html lang="en" class="no-js">
+<!doctype html>
+<html lang="en" class="no-js">
 
-	<head>
-		<meta charset="UTF-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-		<meta name="description" content="">
-		<meta name="author" content="">
-		<meta name="theme-color" content="#3e454c">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="theme-color" content="#3e454c">
 
-		<title>Find a Room | Owner Dashboard</title>
+    <title>Find a Room | Owner Dashboard</title>
 
-		<link rel="stylesheet" href="./assets/css/bootstrap.min.css" type="text/css">
-		<link rel="stylesheet" href="./assets/css/style.css" type="text/css">
-		<link rel="stylesheet" href="./assets/css/owl.carousel.css" type="text/css">
-		<link rel="stylesheet" href="./assets/css/owl.transitions.css" type="text/css">
-		<link href="./assets/css/slick.css" rel="stylesheet">
-		<link href="./assets/css/bootstrap-slider.min.css" rel="stylesheet">
-		<link href="./assets/css/font-awesome.min.css" rel="stylesheet">
-		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="./assets/images/favicon-icon/apple-touch-icon-144-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="./assets/images/favicon-icon/apple-touch-icon-114-precomposed.html">
-		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="./assets/images/favicon-icon/apple-touch-icon-72-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" href="./assets/images/favicon-icon/apple-touch-icon-57-precomposed.png">
-		<link rel="shortcut icon" href="./assets/images/favicon-icon/favicon.png">
-		<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="./assets/css/style.css" type="text/css">
+    <link rel="stylesheet" href="./assets/css/owl.carousel.css" type="text/css">
+    <link rel="stylesheet" href="./assets/css/owl.transitions.css" type="text/css">
+    <link href="./assets/css/slick.css" rel="stylesheet">
+    <link href="./assets/css/bootstrap-slider.min.css" rel="stylesheet">
+    <link href="./assets/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144"
+        href="./assets/images/favicon-icon/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114"
+        href="./assets/images/favicon-icon/apple-touch-icon-114-precomposed.html">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72"
+        href="./assets/images/favicon-icon/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="./assets/images/favicon-icon/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="./assets/images/favicon-icon/favicon.png">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
 
 
 
-		<!-- Font awesome -->
-		<link rel="stylesheet" href="css/font-awesome.min.css">
-		<!-- Sandstone Bootstrap CSS -->
-		<link rel="stylesheet" href="css/bootstrap.min.css">
-		<!-- Bootstrap Datatables -->
-		<link rel="stylesheet" href="css/dataTables.bootstrap.min.css">
-		<!-- Bootstrap social button library -->
-		<link rel="stylesheet" href="css/bootstrap-social.css">
-		<!-- Bootstrap select -->
-		<link rel="stylesheet" href="css/bootstrap-select.css">
-		<!-- Bootstrap file input -->
-		<link rel="stylesheet" href="css/fileinput.min.css">
-		<!-- Awesome Bootstrap checkbox -->
-		<link rel="stylesheet" href="css/awesome-bootstrap-checkbox.css">
-		<!-- owner Stye -->
-		<link rel="stylesheet" href="css/style.css">
-	</head>
+    <!-- Font awesome -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <!-- Sandstone Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Bootstrap Datatables -->
+    <link rel="stylesheet" href="css/dataTables.bootstrap.min.css">
+    <!-- Bootstrap social button library -->
+    <link rel="stylesheet" href="css/bootstrap-social.css">
+    <!-- Bootstrap select -->
+    <link rel="stylesheet" href="css/bootstrap-select.css">
+    <!-- Bootstrap file input -->
+    <link rel="stylesheet" href="css/fileinput.min.css">
+    <!-- Awesome Bootstrap checkbox -->
+    <link rel="stylesheet" href="css/awesome-bootstrap-checkbox.css">
+    <!-- owner Stye -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
 
-	<body>
-		<?php include('includes/header.php'); ?>
+<body>
+    <?php include('includes/header.php'); ?>
 
-		<div class="ts-main-content">
-			<?php include('includes/leftbar.php'); ?>
-			<div class="content-wrapper">
-				<div class="container-fluid">
+    <div class="ts-main-content">
+        <?php include('includes/leftbar.php'); ?>
+        <div class="content-wrapper">
+            <div class="container-fluid">
 
-					<div class="row">
-						<div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-12">
 
-							<h2 class="page-title">Dashboard</h2>
+                        <h2 class="page-title">Dashboard</h2>
 
-							<div class="row">
-								<div class="col-md-12">
-									<div class="row">
-										<div class="col-md-3">
-											<div class="panel panel-default">
-												<div class="panel-body bk-success text-light">
-													<div class="stat-panel text-center">
-														<?php
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <div class="panel panel-default">
+                                            <div class="panel-body bk-success text-light">
+                                                <div class="stat-panel text-center">
+                                                    <?php
 														$sql1 = "SELECT id from tblvehicles ";
 														$query1 = $dbh->prepare($sql1);;
 														$query1->execute();
 														$results1 = $query1->fetchAll(PDO::FETCH_OBJ);
 														$totalvehicle = $query1->rowCount();
 														?>
-														<div class="stat-panel-number h1 "><?php echo htmlentities($totalvehicle); ?></div>
-														<div class="stat-panel-title text-uppercase">Listed Rooms</div>
-													</div>
-												</div>
-												<a href="manage-vehicles.php" class="block-anchor panel-footer text-center">Full Detail &nbsp; <i class="fa fa-arrow-right"></i></a>
-											</div>
-										</div>
-										<div class="col-md-3">
-											<div class="panel panel-default">
-												<div class="panel-body bk-info text-light">
-													<div class="stat-panel text-center">
-														<?php
+                                                    <div class="stat-panel-number h1 ">
+                                                        <?php echo htmlentities($totalvehicle); ?></div>
+                                                    <div class="stat-panel-title text-uppercase">Listed Rooms</div>
+                                                </div>
+                                            </div>
+                                            <a href="manage-vehicles.php"
+                                                class="block-anchor panel-footer text-center">Full Detail &nbsp; <i
+                                                    class="fa fa-arrow-right"></i></a>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="panel panel-default">
+                                            <div class="panel-body bk-info text-light">
+                                                <div class="stat-panel text-center">
+                                                    <?php
 														$sql2 = "SELECT id from tblbooking ";
 														$query2 = $dbh->prepare($sql2);
 														$query2->execute();
@@ -99,117 +105,123 @@ if (strlen($_SESSION['ologin']) == 0) {
 														$bookings = $query2->rowCount();
 														?>
 
-														<div class="stat-panel-number h1 "><?php echo htmlentities($bookings); ?></div>
-														<div class="stat-panel-title text-uppercase">Total Bookings</div>
-													</div>
-												</div>
-												<a href="manage-bookings.php" class="block-anchor panel-footer text-center">Full Detail &nbsp; <i class="fa fa-arrow-right"></i></a>
-											</div>
-										</div>
-										<div class="col-md-3">
-											<div class="panel panel-default">
-												<div class="panel-body bk-warning text-light">
-													<div class="stat-panel text-center">
-														<?php
+                                                    <div class="stat-panel-number h1 ">
+                                                        <?php echo htmlentities($bookings); ?></div>
+                                                    <div class="stat-panel-title text-uppercase">Total Bookings</div>
+                                                </div>
+                                            </div>
+                                            <a href="manage-bookings.php"
+                                                class="block-anchor panel-footer text-center">Full Detail &nbsp; <i
+                                                    class="fa fa-arrow-right"></i></a>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="panel panel-default">
+                                            <div class="panel-body bk-warning text-light">
+                                                <div class="stat-panel text-center">
+                                                    <?php
 														$sql3 = "SELECT id from tblbrands ";
 														$query3 = $dbh->prepare($sql3);
 														$query3->execute();
 														$results3 = $query3->fetchAll(PDO::FETCH_OBJ);
 														$brands = $query3->rowCount();
 														?>
-														<div class="stat-panel-number h1 "><?php echo htmlentities($brands); ?></div>
-														<div class="stat-panel-title text-uppercase">Listed Apartments</div>
-													</div>
-												</div>
-												<a href="manage-brands.php" class="block-anchor panel-footer text-center">Full Detail &nbsp; <i class="fa fa-arrow-right"></i></a>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
+                                                    <div class="stat-panel-number h1 ">
+                                                        <?php echo htmlentities($brands); ?></div>
+                                                    <div class="stat-panel-title text-uppercase">Listed Apartments</div>
+                                                </div>
+                                            </div>
+                                            <a href="manage-brands.php"
+                                                class="block-anchor panel-footer text-center">Full Detail &nbsp; <i
+                                                    class="fa fa-arrow-right"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
 
 
-					<div class="row">
-						<div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-12">
 
 
-							<div class="row">
-								<div class="col-md-12">
-									<div class="row">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="row">
 
 
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-
-
-
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
 
 
 
 
 
-				</div>
-			</div>
-		</div>
-
-		<!-- Loading Scripts -->
-		<script src="js/jquery.min.js"></script>
-		<script src="js/bootstrap-select.min.js"></script>
-		<script src="js/bootstrap.min.js"></script>
-		<script src="js/jquery.dataTables.min.js"></script>
-		<script src="js/dataTables.bootstrap.min.js"></script>
-		<script src="js/Chart.min.js"></script>
-		<script src="js/fileinput.js"></script>
-		<script src="js/chartData.js"></script>
-		<script src="js/main.js"></script>
 
 
-		<?php include('includes/forgotpassword.php'); ?>
-		<!--/Forgot-password-Form -->
 
-		<!-- Scripts -->
-		<script src="assets/js/jquery.min.js"></script>
-		<script src="assets/js/bootstrap.min.js"></script>
-		<script src="assets/js/interface.js"></script>
-		<script src="assets/js/bootstrap-slider.min.js"></script>
-		<!--Slider-JS-->
-		<script src="assets/js/slick.min.js"></script>
-		<script src="assets/js/owl.carousel.min.js"></script>
+            </div>
+        </div>
+    </div>
 
-		<script>
-			window.onload = function() {
+    <!-- Loading Scripts -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap-select.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.dataTables.min.js"></script>
+    <script src="js/dataTables.bootstrap.min.js"></script>
+    <script src="js/Chart.min.js"></script>
+    <script src="js/fileinput.js"></script>
+    <script src="js/chartData.js"></script>
+    <script src="js/main.js"></script>
 
-				// Line chart from swirlData for dashReport
-				var ctx = document.getElementById("dashReport").getContext("2d");
-				window.myLine = new Chart(ctx).Line(swirlData, {
-					responsive: true,
-					scaleShowVerticalLines: false,
-					scaleBeginAtZero: true,
-					multiTooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value %>",
-				});
 
-				// Pie Chart from doughutData
-				var doctx = document.getElementById("chart-area3").getContext("2d");
-				window.myDoughnut = new Chart(doctx).Pie(doughnutData, {
-					responsive: true
-				});
+    <?php include('includes/forgotpassword.php'); ?>
+    <!--/Forgot-password-Form -->
 
-				// Dougnut Chart from doughnutData
-				var doctx = document.getElementById("chart-area4").getContext("2d");
-				window.myDoughnut = new Chart(doctx).Doughnut(doughnutData, {
-					responsive: true
-				});
+    <!-- Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/interface.js"></script>
+    <script src="assets/js/bootstrap-slider.min.js"></script>
+    <!--Slider-JS-->
+    <script src="assets/js/slick.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
 
-			}
-		</script>
-	</body>
+    <script>
+    window.onload = function() {
 
-	</html>
+        // Line chart from swirlData for dashReport
+        var ctx = document.getElementById("dashReport").getContext("2d");
+        window.myLine = new Chart(ctx).Line(swirlData, {
+            responsive: true,
+            scaleShowVerticalLines: false,
+            scaleBeginAtZero: true,
+            multiTooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value %>",
+        });
+
+        // Pie Chart from doughutData
+        var doctx = document.getElementById("chart-area3").getContext("2d");
+        window.myDoughnut = new Chart(doctx).Pie(doughnutData, {
+            responsive: true
+        });
+
+        // Dougnut Chart from doughnutData
+        var doctx = document.getElementById("chart-area4").getContext("2d");
+        window.myDoughnut = new Chart(doctx).Doughnut(doughnutData, {
+            responsive: true
+        });
+
+    }
+    </script>
+</body>
+
+</html>
 <?php } ?>
