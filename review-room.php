d0b408f347ab9f27cf29ec0b66ffc2a8df799bee
Ryan <ryanluzentales@gmail.com>
2022-10-27 09:25:38 +0800

    reviews
    reviews

--- review-room.php
+++ review-room.php
@@ -30,8 +30,8 @@ error_reporting(0);
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
     <script src="jquery-3.2.1.min.js"></script>
 
-    
-    
+
+
 </head>
 
 <body>
@@ -42,120 +42,126 @@ error_reporting(0);
     <!-- /Header -->
 
 
-	<div class="comment-form-container">
-		<form id="frm-comment">
-			<div class="input-row">
-				<div class="label">Name:</div>
-				<input type="hidden" name="comment_id" id="commentId" /> <input
-					class="input-field" type="text" name="name" id="name"
-					placeholder="Enter your name" />
-			</div>
-			<div class="input-row">
-				<textarea class="input-field" name="comment" id="comment"
-					placeholder="Your comment here"></textarea>
-			</div>
-			<div>
-				<input type="button" class="btn-submit" id="submitButton"
-					value="Publish" />
-				<div id="comment-message">Comment added successfully.</div>
-			</div>
-		</form>
-	</div>
-	<div id="output"></div>
-	<script>
-            function postReply(commentId) {
-                $('#commentId').val(commentId);
-                $("#name").focus();
-            }
+    <div class="comment-form-container">
+        <form id="frm-comment">
+            <h4 class="review-title">Review Page</h4>
+            <div class="input-row">
+                <div class="label-name">Name:</div><br>
+                <input type="hidden" name="comment_id" id="commentId" /> <input class="input-field-name" type="text"
+                    name="name" id="name" placeholder="Enter your name" />
+            </div>
+           
+            <div class="input-row">
+                <div class="label-comment">Comment:</div><br>
+                <textarea class="input-field-comment" name="comment" id="comment"
+                    placeholder="Your comment here"></textarea>
+            </div>
+        
+            <div>
+                <input type="button" class="comment-btn-submit" id="submitButton" value="Publish" />
+            </div>
+        </form>
+    </div>
+    <div class="display" id="output"></div>
+    <script>
+    function postReply(commentId) {
+        $('#commentId').val(commentId);
+        $("#name").focus();
+    }
 
-            $("#submitButton").click(function () {
-            	   $("#comment-message").css('display', 'none');
-                var str = $("#frm-comment").serialize();
+    $("#submitButton").click(function() {
+        $("#comment-message").css('display', 'none');
+        var str = $("#frm-comment").serialize();
 
-                $.ajax({
-                    url: "comment-add.php",
-                    data: str,
-                    type: 'post',
-                    success: function (response)
-                    {
-                        var result = eval('(' + response + ')');
-                        if (response)
-                        {
-                        	$("#comment-message").css('display', 'inline-block');
-                            $("#name").val("");
-                            $("#comment").val("");
-                            $("#commentId").val("");
-                     	   listComment();
-                        } else
-                        {
-                            alert("Failed to add comments !");
-                            return false;
-                        }
-                    }
-                });
-            });
-            
-            $(document).ready(function () {
-            	   listComment();
-            });
-
-            function listComment() {
-                $.post("comment-list.php",
-                        function (data) {
-                           var data = JSON.parse(data);
-                            
-                            var comments = "";
-                            var replies = "";
-                            var item = "";
-                            var parent = -1;
-                            var results = new Array();
-
-                            var list = $("<ul class='outer-comment'>");
-                            var item = $("<li>").html(comments);
-
-                            for (var i = 0; (i < data.length); i++)
-                            {
-                                var commentId = data[i]['comment_id'];
-                                parent = data[i]['parent_comment_id'];
-
-                                if (parent == "0")
-                                {
-                                    comments = "<div class='comment-row'>"+
-                                    "<div class='comment-info'><span class='comment-row-label'>from</span> <span class='posted-by'>" + data[i]['comment_sender_name'] + " </span> <span class='comment-row-label'>at</span> <span class='posted-at'>" + data[i]['comment_at'] + "</span></div>" + 
-                                    "<div class='comment-text'>" + data[i]['comment'] + "</div>"+
-                                    "<div><a class='btn-reply' onClick='postReply(" + commentId + ")'>Reply</a></div>"+
-                                    "</div>";
-
-                                    var item = $("<li>").html(comments);
-                                    list.append(item);
-                                    var reply_list = $('<ul>');
-                                    item.append(reply_list);
-                                    listReplies(commentId, data, reply_list);
-                                }
-                            }
-                            $("#output").html(list);
-                        });
-            }
-
-            function listReplies(commentId, data, list) {
-                for (var i = 0; (i < data.length); i++)
-                {
-                    if (commentId == data[i].parent_comment_id)
-                    {
-                        var comments = "<div class='comment-row'>"+
-                        " <div class='comment-info'><span class='comment-row-label'>from</span> <span class='posted-by'>" + data[i]['comment_sender_name'] + " </span> <span class='comment-row-label'>at</span> <span class='posted-at'>" + data[i]['comment_at'] + "</span></div>" + 
-                        "<div class='comment-text'>" + data[i]['comment'] + "</div>"+
-                        "<div><a class='btn-reply' onClick='postReply(" + data[i]['comment_id'] + ")'>Reply</a></div>"+
-                        "</div>";
-                        var item = $("<li>").html(comments);
-                        var reply_list = $('<ul>');
-                        list.append(item);
-                        item.append(reply_list);
-                        listReplies(data[i].comment_id, data, reply_list);
-                    }
+        $.ajax({
+            url: "comment-add.php",
+            data: str,
+            type: 'post',
+            success: function(response) {
+                var result = eval('(' + response + ')');
+                if (response) {
+                    $("#comment-message").css('display', 'inline-block');
+                    $("#name").val("");
+                    $("#comment").val("");
+                    $("#commentId").val("");
+                    listComment();
+                } else {
+                    alert("Failed to add comments !");
+                    return false;
                 }
             }
-        </script>
+        });
+    });
+
+    $(document).ready(function() {
+        listComment();
+    });
+
+    function listComment() {
+        $.post("comment-list.php",
+            function(data) {
+                var data = JSON.parse(data);
+
+                var comments = "";
+                var replies = "";
+                var item = "";
+                var parent = -1;
+                var results = new Array();
+
+                var list = $("<ul class='outer-comment'>");
+                var item = $("<li>").html(comments);
+
+                for (var i = 0;
+                    (i < data.length); i++) {
+                    var commentId = data[i]['comment_id'];
+                    parent = data[i]['parent_comment_id'];
+
+                    if (parent == "0") {
+                        comments = "<div class='comment-row'>" +
+                            "<div class='comment-info'><span class='comment-row-label'></span> <span class='posted-by'>" +
+                            data[i]['comment_sender_name'] +
+                            " </span> <span class='comment-row-label'> at </span> <span class='posted-at'>" + data[
+                                i]
+                            [
+                                'comment_at'
+                            ] + "</span></div>" +
+                            "<div class='comment-text'>" + data[i]['comment'] + "</div>" +
+                            "<div><a class='btn-reply' onClick='postReply(" + commentId + ")'>Reply</a></div>" +
+                            "</div>";
+
+                        var item = $("<li>").html(comments);
+                        list.append(item);
+                        var reply_list = $('<ul>');
+                        item.append(reply_list);
+                        listReplies(commentId, data, reply_list);
+                    }
+                }
+                $("#output").html(list);
+            });
+    }
+
+    function listReplies(commentId, data, list) {
+        for (var i = 0;
+            (i < data.length); i++) {
+            if (commentId == data[i].parent_comment_id) {
+                var comments = "<div class='comment-row'>" +
+                    " <div class='comment-info'><span class='comment-row-label'></span> <span class='posted-by'>" +
+                    data[i]['comment_sender_name'] +
+                    " </span> <span class='comment-row-label'>at</span> <span class='posted-at'>" + data[i][
+                        'comment_at'
+                    ] + "</span></div>" +
+                    "<div class='comment-text'>" + data[i]['comment'] + "</div>" +
+                    "<div><a class='btn-reply' onClick='postReply(" + data[i]['comment_id'] + ")'>Reply</a></div>" +
+                    "</div>";
+                var item = $("<li>").html(comments);
+                var reply_list = $('<ul>');
+                list.append(item);
+                item.append(reply_list);
+                listReplies(data[i].comment_id, data, reply_list);
+            }
+        }
+    }
+    </script>
 
 
 
