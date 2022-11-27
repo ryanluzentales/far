4ab798effdcb6596c7ba252a8fce520a2b8b4a46
Ryan <40173480+ryanluzentales@users.noreply.github.com>
2022-11-27 15:53:47 +0800

    done tabs
    done tabs

--- admin/reg-users.php
+++ admin/reg-users.php
@@ -88,109 +88,185 @@ if (strlen($_SESSION['alogin']) == 0) {
                 <!-- Tab content -->
 
                 <div id="London" class="tabcontent">
-                    <br>
-                    <br>
-                    <br>
-                    <h6>Room Owner</h6>
-                    <p>This is the tab for the room owner</p>
-                </div>
+                    <div class="container-fluid">
 
-                <div id="Paris" class="tabcontent">
-                    <br>
-                    <br>
-                    <br>
-                    <h6>Client</h6>
-                    <p>This is the tap for Clients</p>
-                    <p>This is the tap for Clients</p>
-                    <p>This is the tap for Clients</p>
-                </div>
-            </div>
+                        <div class="row">
+                            <div class="col-md-12">
 
-            <div class="container-fluid">
+                                <h2 class="page-title">Registered Users</h2>
 
-                <div class="row">
-                    <div class="col-md-12">
+                                <!-- Zero Configuration Table -->
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">OWNER ACCOUNTS</div>
+                                    <div class="panel-body">
+                                        <?php if ($error) { ?><div class="errorWrap">
+                                            <strong>ERROR</strong>:<?php echo htmlentities($error); ?>
+                                        </div>
+                                        <?php } else if ($msg) { ?><div class="succWrap">
+                                            <strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?>
+                                        </div><?php } ?>
+                                        <table id="zctb" class="display table table-striped table-bordered table-hover"
+                                            cellspacing="0" width="100%">
+                                            <thead>
+                                                <tr>
+                                                    <th>#</th>
+                                                    <th> Name</th>
+                                                    <th>Email </th>
+                                                    <th>Contact no</th>
+                                                    <th>DOB</th>
+                                                    <th>Address</th>
+                                                    <th>City</th>
+                                                    <th>Country</th>
+                                                    <th>Reg Date</th>
 
-                        <h2 class="page-title">Registered Users</h2>
+                                                </tr>
+                                            </thead>
+                                            <tfoot>
+                                                <tr>
+                                                    <th>#</th>
+                                                    <th> Name</th>
+                                                    <th>Email </th>
+                                                    <th>Contact no</th>
+                                                    <th>DOB</th>
+                                                    <th>Address</th>
+                                                    <th>City</th>
+                                                    <th>Country</th>
+                                                    <th>Reg Date</th>
+                                                </tr>
+                                                </tr>
+                                            </tfoot>
+                                            <tbody>
 
-                        <!-- Zero Configuration Table -->
-                        <div class="panel panel-default">
-                            <div class="panel-heading">CLIENT ACCOUNTS</div>
-                            <div class="panel-body">
-                                <?php if ($error) { ?><div class="errorWrap">
-                                    <strong>ERROR</strong>:<?php echo htmlentities($error); ?>
-                                </div>
-                                <?php } else if ($msg) { ?><div class="succWrap">
-                                    <strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?>
-                                </div><?php } ?>
-                                <table id="zctb" class="display table table-striped table-bordered table-hover"
-                                    cellspacing="0" width="100%">
-                                    <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th> Name</th>
-                                            <th>Email </th>
-                                            <th>Contact no</th>
-                                            <th>DOB</th>
-                                            <th>Address</th>
-                                            <th>City</th>
-                                            <th>Country</th>
-                                            <th>Reg Date</th>
-
-                                        </tr>
-                                    </thead>
-                                    <tfoot>
-                                        <tr>
-                                            <th>#</th>
-                                            <th> Name</th>
-                                            <th>Email </th>
-                                            <th>Contact no</th>
-                                            <th>DOB</th>
-                                            <th>Address</th>
-                                            <th>City</th>
-                                            <th>Country</th>
-                                            <th>Reg Date</th>
-                                        </tr>
-                                        </tr>
-                                    </tfoot>
-                                    <tbody>
-
-                                        <?php $sql = "SELECT * from  tblusers ";
+                                                <?php $sql = "SELECT * from  tblusers ";
 											$query = $dbh->prepare($sql);
 											$query->execute();
 											$results = $query->fetchAll(PDO::FETCH_OBJ);
 											$cnt = 1;
 											if ($query->rowCount() > 0) {
 												foreach ($results as $result) {				?>
-                                        <tr>
-                                            <td><?php echo htmlentities($cnt); ?></td>
-                                            <td><?php echo htmlentities($result->FullName); ?></td>
-                                            <td><?php echo htmlentities($result->EmailId); ?></td>
-                                            <td><?php echo htmlentities($result->ContactNo); ?></td>
-                                            <td><?php echo htmlentities($result->dob); ?></td>
-                                            <td><?php echo htmlentities($result->Address); ?></td>
-                                            <td><?php echo htmlentities($result->City); ?></td>
-                                            <td><?php echo htmlentities($result->Country); ?></td>
-                                            <td><?php echo htmlentities($result->RegDate); ?></td>
-                                        </tr>
-                                        <?php $cnt = $cnt + 1;
+                                                <tr>
+                                                    <td><?php echo htmlentities($cnt); ?></td>
+                                                    <td><?php echo htmlentities($result->FullName); ?></td>
+                                                    <td><?php echo htmlentities($result->EmailId); ?></td>
+                                                    <td><?php echo htmlentities($result->ContactNo); ?></td>
+                                                    <td><?php echo htmlentities($result->dob); ?></td>
+                                                    <td><?php echo htmlentities($result->Address); ?></td>
+                                                    <td><?php echo htmlentities($result->City); ?></td>
+                                                    <td><?php echo htmlentities($result->Country); ?></td>
+                                                    <td><?php echo htmlentities($result->RegDate); ?></td>
+                                                </tr>
+                                                <?php $cnt = $cnt + 1;
 												}
 											} ?>
 
-                                    </tbody>
-                                </table>
+                                            </tbody>
+                                        </table>
+
+
+
+                                    </div>
+                                </div>
 
 
 
                             </div>
                         </div>
 
-
-
                     </div>
                 </div>
 
+                <div id="Paris" class="tabcontent">
+
+                    <div class="container-fluid">
+
+                        <div class="row">
+                            <div class="col-md-12">
+
+                                <h2 class="page-title">Registered Users</h2>
+
+                                <!-- Zero Configuration Table -->
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">CLIENT ACCOUNTS</div>
+                                    <div class="panel-body">
+                                        <?php if ($error) { ?><div class="errorWrap">
+                                            <strong>ERROR</strong>:<?php echo htmlentities($error); ?>
+                                        </div>
+                                        <?php } else if ($msg) { ?><div class="succWrap">
+                                            <strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?>
+                                        </div><?php } ?>
+                                        <table id="zctb" class="display table table-striped table-bordered table-hover"
+                                            cellspacing="0" width="100%">
+                                            <thead>
+                                                <tr>
+                                                    <th>#</th>
+                                                    <th> Name</th>
+                                                    <th>Email </th>
+                                                    <th>Contact no</th>
+                                                    <th>DOB</th>
+                                                    <th>Address</th>
+                                                    <th>City</th>
+                                                    <th>Country</th>
+                                                    <th>Reg Date</th>
+
+                                                </tr>
+                                            </thead>
+                                            <tfoot>
+                                                <tr>
+                                                    <th>#</th>
+                                                    <th> Name</th>
+                                                    <th>Email </th>
+                                                    <th>Contact no</th>
+                                                    <th>DOB</th>
+                                                    <th>Address</th>
+                                                    <th>City</th>
+                                                    <th>Country</th>
+                                                    <th>Reg Date</th>
+                                                </tr>
+                                                </tr>
+                                            </tfoot>
+                                            <tbody>
+
+                                                <?php $sql = "SELECT * from  tblusers ";
+											$query = $dbh->prepare($sql);
+											$query->execute();
+											$results = $query->fetchAll(PDO::FETCH_OBJ);
+											$cnt = 1;
+											if ($query->rowCount() > 0) {
+												foreach ($results as $result) {				?>
+                                                <tr>
+                                                    <td><?php echo htmlentities($cnt); ?></td>
+                                                    <td><?php echo htmlentities($result->FullName); ?></td>
+                                                    <td><?php echo htmlentities($result->EmailId); ?></td>
+                                                    <td><?php echo htmlentities($result->ContactNo); ?></td>
+                                                    <td><?php echo htmlentities($result->dob); ?></td>
+                                                    <td><?php echo htmlentities($result->Address); ?></td>
+                                                    <td><?php echo htmlentities($result->City); ?></td>
+                                                    <td><?php echo htmlentities($result->Country); ?></td>
+                                                    <td><?php echo htmlentities($result->RegDate); ?></td>
+                                                </tr>
+                                                <?php $cnt = $cnt + 1;
+												}
+											} ?>
+
+                                            </tbody>
+                                        </table>
+
+
+
+                                    </div>
+                                </div>
+
+
+
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
             </div>
+
+
         </div>
 
 
